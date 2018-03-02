ee836ef87b2ea61cf4b5ce635e36fed1f6c60aa7
ErikBoesen <me@erikboesen.com>
2018-03-02 12:06:41 -0500

    Ligili dosierojn de ia dosierujo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-for f in $(ls -A1 $HOME/.files/files); do
-    echo $f...
+for f in $(cd $HOME/.files/files; find . -type f); do
     ln -si $HOME/.files/files/$f $HOME/$f
 done
