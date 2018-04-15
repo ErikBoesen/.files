c415c391cee37500c466748d7feb4b95b97ecc45
ErikBoesen <me@erikboesen.com>
2018-04-15 10:51:06 -0400

    chmod 600 all private files after writing

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ for f in $(cd $HOME/.files/files; find . -type f); do
     bn=$(basename "$f")
     if [[ "$bn" == ".example"* ]]; then
         vim $HOME/${bn#.example}
+        chmod 600 $HOME/${bn#.example}
     else
         ln -si $HOME/.files/files/$f $HOME/$f
     fi
