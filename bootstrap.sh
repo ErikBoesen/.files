95ea7b31a7c34951e096bd90c9495926fea508f2
ErikBoesen <me@erikboesen.com>
2018-04-15 11:54:28 -0400

    Copy private files for writing

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 for f in $(cd $HOME/.files/files; find . -type f); do
     bn=$(basename "$f")
     if [[ "$bn" == ".example"* ]]; then
+        cp "$HOME/.files/files/$f" $HOME/${bn#.example}
         vim $HOME/${bn#.example}
         chmod 600 $HOME/${bn#.example}
     else
