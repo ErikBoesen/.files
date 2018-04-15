26372d9ac1477ab12cca2cc07b8c8f9958b6099b
ErikBoesen <me@erikboesen.com>
2018-04-15 17:59:40 -0400

    Use dfroot constant in bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-for f in $(cd $HOME/.files/files; find . -type f); do
+dfroot="$(dirname $(realpath $0))/files"
+
+for f in $(cd $dfroot; find . -type f); do
     bn=$(basename "$f")
     if [[ "$bn" == ".example"* ]] && ! [[ -e $HOME/${bn#.example} ]]; then
         cp "$HOME/.files/files/$f" $HOME/${bn#.example}
         vim $HOME/${bn#.example}
         chmod 600 $HOME/${bn#.example}
     else
-        ln -si $HOME/.files/files/$f $HOME/$f
+        ln -si $dfroot/$f $HOME/$f
     fi
 done
