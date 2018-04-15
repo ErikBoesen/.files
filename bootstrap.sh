5154fe294cbdb9efb32e1fa557a6beb4fde7fa2c
ErikBoesen <me@erikboesen.com>
2018-04-15 10:45:56 -0400

    Invite input to files starting with `.example`

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 for f in $(cd $HOME/.files/files; find . -type f); do
-    ln -si $HOME/.files/files/$f $HOME/$f
+    bn=$(basename "$f")
+    if [[ "$bn" == ".example"* ]]; then
+        vim $HOME/${bn#.example}
+    else
+        ln -si $HOME/.files/files/$f $HOME/$f
+    fi
 done
