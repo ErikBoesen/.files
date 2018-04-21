7ce83e62a2c2fe9b94bdcbada9d99d776b9fbfef
ErikBoesen <me@erikboesen.com>
2018-04-20 23:46:49 -0400

    Fix private file copying

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-dfroot="$(dirname $(realpath $0))/files"
+function install {
+    dir="$1"
+    for f in $(cd $dir; find . -type f); do
+        bn=$(basename "$f")
+        if [[ "$bn" == ".example"* ]]; then
+            if ! [[ -e $HOME/$(dirname "$f")/${bn#.example} ]]; then
+                cp "$dir/$f" $HOME/$(dirname "$f")/${bn#.example}
+                vim $HOME/${bn#.example}
+                chmod 600 $HOME/${bn#.example}
+            else
+                echo "Ya existe archivo privado ${bn#.example}."
+            fi
+        else
+            ln -si $dir/${f#./} $HOME/${f#./}
+        fi
+    done
+}
 
 for f in $(cd $dfroot; find . -type f); do
     bn=$(basename "$f")
