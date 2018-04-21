371e78aca828b402bbc24b0f2cc74b712f6457d0
ErikBoesen <me@erikboesen.com>
2018-04-20 23:46:49 -0400

    Create install function to install all dotfiles in folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,13 +18,5 @@ function install {
     done
 }
 
-for f in $(cd $dfroot; find . -type f); do
-    bn=$(basename "$f")
-    if [[ "$bn" == ".example"* ]] && ! [[ -e $HOME/${bn#.example} ]]; then
-        cp "$HOME/.files/files/$f" $HOME/${bn#.example}
-        vim $HOME/${bn#.example}
-        chmod 600 $HOME/${bn#.example}
-    else
-        ln -si $dfroot/${f#./} $HOME/${f#./}
-    fi
-done
+dfroot="$(dirname $(realpath $0))"
+install $dfroot/files
