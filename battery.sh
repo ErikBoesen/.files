4b3ba93d243c2405dce2e897b028dd8eb4e374d5
ErikBoesen <me@erikboesen.com>
2018-07-17 10:40:36 -0400

    Add colors to battery.sh

--- battery.sh
+++ battery.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-printf "+-: "
-ioreg -l | awk '$3~/Capacity/{c[$3]=$5}END{OFMT="%.2f";max=c["\"MaxCapacity\""];print(max>0?100*c["\"CurrentCapacity\""]/max:"?")}'
+
+battery="$(ioreg -l | awk '$3~/Capacity/{c[$3]=$5}END{OFMT="%.0f";max=c["\"MaxCapacity\""];print(max>0?100*c["\"CurrentCapacity\""]/max:"?")}')"
+# Returns "Yes" or "No"
+charging="$(ioreg -l | grep "IsCharging" | awk '{print $5}')"
+if [[ $charging == "Yes" ]]; then
+    printf "\e[34m"
+elif [[ "$battery" -gt 75 ]]; then
+    printf "\e[32m"
+elif [[ "$battery" -gt 50 ]]; then
+    printf "\e[33m"
+elif [[ "$battery" -gt 25 ]]; then
+    printf "\e[1;38;5;202m"
+else
+    printf "\e[31m"
+fi
+printf "${battery}%%\e[0m"
