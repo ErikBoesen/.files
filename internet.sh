657372a8db030ae0cf8b28d0aa992303259e3059
ErikBoesen <me@erikboesen.com>
2018-07-21 14:17:20 -0400

    internet.sh: show network properly

--- internet.sh
+++ internet.sh
@@ -2,9 +2,12 @@
 wifi_network="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | sed 's/.*SSID: //'
 ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
 
-# TODO: Differentiate between ethernet and WiFi connection
-if [[ -z $wifi_network ]]; then
+if [[ -z $ip ]]; then
     printf "Ø"
 else
-    printf "☰ $ip - $wifi_network"
+    printf "☰ $ip"
+fi
+
+if ! [[ -z $wifi_network ]]; then
+    printf " - $wifi_network"
 fi
