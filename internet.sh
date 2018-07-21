cc9e129c0f71fc2dcbe4c6fd95986f8017e93a53
ErikBoesen <me@erikboesen.com>
2018-07-21 14:11:49 -0400

    internet.sh: get WiFi network name

--- internet.sh
+++ internet.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-wifi_status="$(networksetup -getairportpower en0 | awk '{print $4}')"
+wifi_network="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | sed 's/.*SSID: //'
 ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
 
 # TODO: Differentiate between ethernet and WiFi connection
-# TODO: Differentiate between actual internet connection and WiFi just being on
-if [[ $wifi_status == "On" ]]; then
-    printf "☰ $ip"
-else
+if [[ -z $wifi_network ]]; then
     printf "Ø"
+else
+    printf "☰ $ip - $wifi_network"
 fi
