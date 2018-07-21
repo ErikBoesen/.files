6ed5680354763282301e552fedc4399e0444ccae
ErikBoesen <me@erikboesen.com>
2018-07-21 14:21:14 -0400

    internet.sh: fix output

--- internet.sh
+++ internet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wifi_network="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | sed 's/.*SSID: //'
+wifi_network="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep " SSID" | sed 's/.*SSID: //')"
 ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
 
 if [[ -z $ip ]]; then
