b7d6c4bbb366d069432feb46fe7e89772aa416df
ErikBoesen <me@erikboesen.com>
2018-07-28 22:54:02 -0400

    track.sh: sanity check file types

--- track.sh
+++ track.sh
@@ -12,6 +12,12 @@ echo "Source: $source"
 echo "Destination: $dest"
 echo "Script directory: $script_dir"
 
+echo "Sanity checking file type..."
+if [[ ! -f "$source" ]]; then
+    echo "$source is not a regular file! Terminating." >&2
+    exit 1
+fi
+
 mv "$source" "$dest"
 ln -s "$dest" "$source"
 
