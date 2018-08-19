d8c62a8ef79ba0be6163c7598b144f610ef10d56
ErikBoesen <me@erikboesen.com>
2018-08-19 13:53:24 -0400

    temp.py: change background colors too

--- temp.py
+++ temp.py
@@ -2,19 +2,19 @@
 
 # Temperature thresholds
 CRITICAL = 80
-DANGER   = 60
+DANGER   = 20
 WARNING  = 40
 
 with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
     temperature = float(f.read()) / 1000
 
 if temperature > CRITICAL:
-    colour = 'fg=red'
+    colour = 'fg=red,blink'
 elif temperature > DANGER:
-    colour = 'fg=colour202'
+    colour = 'bg=colour202'
 elif temperature > WARNING:
     colour = 'fg=yellow'
 else:
     colour = 'fg=blue'
 
-print(f'#[{colour}]{temperature}°C')
+print(f'#[{colour}]{temperature}°C#[bg=default,fg=default,noblink]')
