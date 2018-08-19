4c66b94eed6b0cd116f34d2166056b2bca5bf352
ErikBoesen <me@erikboesen.com>
2018-08-19 13:43:52 -0400

    temp.py: allow changing more color attributes cleanly

--- temp.py
+++ temp.py
@@ -9,12 +9,12 @@ with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
     temperature = float(f.read()) / 1000
 
 if temperature > CRITICAL:
-    colour = 'red'
+    colour = 'fg=red'
 elif temperature > DANGER:
-    colour = 'colour202'
+    colour = 'fg=colour202'
 elif temperature > WARNING:
-    colour = 'yellow'
+    colour = 'fg=yellow'
 else:
-    colour = 'blue'
+    colour = 'fg=blue'
 
-print(f'#[fg={colour}]{temperature}°C')
+print(f'#[{colour}]{temperature}°C')
