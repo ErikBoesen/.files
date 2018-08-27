7030b1e75a608e1ef146ea078ad601c85290b67f
ErikBoesen <me@erikboesen.com>
2018-08-27 11:27:08 -0400

    temp.py: hide unnecessary decimal place

--- temp.py
+++ temp.py
@@ -6,8 +6,10 @@ DANGER   = 60
 WARNING  = 40
 
 # Thermal zones 0 and 1 do not seem to change.
+# TODO: More investigation of this phenomenon needed.
+# TODO: Show decimal (doesn't vary in this file)
 with open('/sys/class/thermal/thermal_zone2/temp', 'r') as f:
-    temperature = float(f.read()) / 1000
+    temperature = int(f.read()) // 1000
 
 if temperature > CRITICAL:
     colour = 'fg=red,blink'
