df92128e531e11b7f7d8309fccb2e2bcf9fc0e17
ErikBoesen <me@erikboesen.com>
2019-06-13 11:16:18 -0400

    Use xargs to put all IPs on same line

--- internet.sh
+++ internet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wifi_network="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep " SSID" | sed 's/.*SSID: //')"
-ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
+ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | xargs)"
 
 if [[ -z $ip ]]; then
     printf "Ø"
