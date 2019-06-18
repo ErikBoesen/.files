614b100dfa97f8ae9d45849371d0cf9a0ae7e8f8
ErikBoesen <me@erikboesen.com>
2019-06-18 11:09:22 -0400

    conlan.sh: delete

--- conlan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for prog in MOOSDB cat curl emacs open tmux vim zsh; do
-    ln -s "$(which sl)" ~/.local/bin/"$prog"
-done
-
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
