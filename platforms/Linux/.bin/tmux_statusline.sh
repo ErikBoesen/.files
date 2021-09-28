de3ec761d5c49e5c1e2f13a540845c0986741716
ErikBoesen <me@erikboesen.com>
2021-09-28 16:23:44 -0400

    platforms/Linux/.bin/tmux_statusline.sh: rewrite statusline script in Python and add animal emoji

--- platforms/Linux/.bin/tmux_statusline.sh
+++ platforms/Linux/.bin/tmux_statusline.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-viper
-hornet
-cricket
-newt
-termite
-python
-cicada
-bumblebee
-cobra
-frog
-tick
-turtle
-aphid
-ladybug
-chameleon
-rattlesnake
-scorpion
-perch
-dolphin
-kangaroo
-hippo
-swan
-gator
-jaguar
-rhino
-grizzly
-macaw
-monkey
-cardinal
-peacock
-lion
-giraffee
-hare
-raven
-tiger
-hawk
-woodpecker
-zebra
-
-host=$(hostname | awk -F. '{print $1}')
-echo "#[fg=yellow]☾ $host"
+python3 <<'EOF'
+animal_emotes = {
+    'viper': '🐍',
+    'hornet': '🐝',
+    'cricket': '🪳',
+    'newt': '🦎',
+    'termite': '🐜',
+    'python': '🐍',
+    'cicada': '🪰',
+    'bumblebee': '🐝',
+    'cobra': '🐍',
+    'frog': '🐸',
+    'tick': '🪳',
+    'turtle': '🐢',
+    'aphid': '🪲',
+    'ladybug': '🐞',
+    'chameleon': '🦎',
+    'rattlesnake': '🐍',
+    'scorpion': '🦂',
+    'perch': '🐟',
+    'dolphin': '🐬',
+    'kangaroo': '🦘',
+    'hippo': '🦛',
+    'swan': '🦢',
+    'gator': '🐊',
+    'jaguar': '🐆',
+    'rhino': '🦏',
+    'grizzly': '🐻',
+    'macaw': '🐦',
+    'monkey': '🐵',
+    'cardinal': '🦩',
+    'peacock': '🦚',
+    'lion': '🦁',
+    'giraffee': '🦒',
+    'hare': '🐇',
+    'raven': '🐧',
+    'tiger': '🐅',
+    'hawk': '🦅',
+    'woodpecker': '🦤',
+    'zebra': '🦓',
+}
+import socket
+host = socket.gethostname().split('.')[0]
+emoji = animal_emotes.get(host, '☾')
+print('#[fg=yellow]' + emoji + ' ' + host)
+EOF
 
 #status=""
 #div=" #[fg=colour240]// "
