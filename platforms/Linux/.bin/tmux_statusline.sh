13abb108d2470e25a43f13a2c97b6d4a32154c9e
ErikBoesen <me@erikboesen.com>
2021-09-28 16:09:37 -0400

    platforms/Linux/.bin/tmux_statusline.sh: show only hostname in statusline on linux

--- platforms/Linux/.bin/tmux_statusline.sh
+++ platforms/Linux/.bin/tmux_statusline.sh
@@ -1,16 +1,58 @@
 #!/bin/bash
 
-status=""
-div=" #[fg=colour240]// "
+viper
+hornet
+cricket
+newt
+termite
+python
+cicada
+bumblebee
+cobra
+frog
+tick
+turtle
+aphid
+ladybug
+chameleon
+rattlesnake
+scorpion
+perch
+dolphin
+kangaroo
+hippo
+swan
+gator
+jaguar
+rhino
+grizzly
+macaw
+monkey
+cardinal
+peacock
+lion
+giraffee
+hare
+raven
+tiger
+hawk
+woodpecker
+zebra
 
-spotify="$(spotify.py)"
-if ! [[ -z $spotify ]]; then
-    status+="#[fg=colour2]${spotify}${div}"
-fi
+host=$(hostname | awk -F. '{print $1}')
+echo "#[fg=yellow]☾ $host"
 
-status+="#[fg=blue]$(internet.sh)${div}"
-status+="$(temp.py)${div}"
-# TODO: Use real analog clock
-status+="#[fg=red]◶"
+#status=""
+#div=" #[fg=colour240]// "
 
-echo "$status"
+#spotify="$(spotify.py)"
+#if ! [[ -z $spotify ]]; then
+#    status+="#[fg=colour2]${spotify}${div}"
+#fi
+#
+#status+="#[fg=blue]$(internet.sh)${div}"
+#status+="$(temp.py)${div}"
+## TODO: Use real analog clock
+#status+="#[fg=red]◶"
+
+#echo "$status"
